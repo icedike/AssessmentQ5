c432612f507cf6faf42ebf455b14b5529f518425
icedike <icedike@gmail.com>
2017-01-10 14:44:30 +0800

    first commit

---
+++ AssessmentQ5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssessmentQ5
+//
+//  Created by LIN TINGMIN on 10/01/2017.
+//  Copyright © 2017 MarkRobotDesignTEST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
